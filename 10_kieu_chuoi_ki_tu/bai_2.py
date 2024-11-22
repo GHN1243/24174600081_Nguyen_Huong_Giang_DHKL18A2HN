cfa975bb86bdcdfbc3df378a5e9470da2a0131bd
Nguyễn Hương Giang DHKL18A2HN <huonggiang06089@gmail.com>
2024-11-23 00:58:26 +0700

    10_kieu_chuoi_ki_tu

--- 10_kieu_chuoi_ki_tu/bai_2.py
+++ 10_kieu_chuoi_ki_tu/bai_2.py
@@ -2,11 +2,6 @@
 # Ví dụ: Nhập vào: “Hom nay    troi   mua   ”
 #              Trả về: “Hom nay troi mua”
 
-# Nhập chuỗi từ người dùng
 chuoi_ki_tu_ban_dau  = input("Nhập vào chuỗi ký tự: ")
-
-# Loại bỏ dấu cách thừa (dùng split để tách các từ và join để nối lại)
 chuoi_sau_khi_xoa = " ".join(chuoi_ki_tu_ban_dau.split())
-
-# In ra chuỗi kết quả
 print("Chuỗi sau khi loại bỏ dấu cách thừa:", chuoi_sau_khi_xoa )
