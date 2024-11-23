88ba177f45c83d78943e53796234d831ba962056
Nguyễn Hương Giang DHKL18A2HN <huonggiang06089@gmail.com>
2024-11-23 18:13:34 +0700

    10_kieu_chuoi_ki_tu

--- bai_kiem_tra/bai_1.py
+++ bai_kiem_tra/bai_1.py
@@ -11,10 +11,10 @@ k = float(input("Nhập tung độ của tâm đường tròn (k): "))
 r = float(input("Nhập bán kính của đường tròn (r): "))
 
 # Tính khoảng cách từ tâm của đường tròn (h, k) đến đường thẳng Ax + By + C = 0
-distance = abs(A * h + B * k + C) / math.sqrt(A**2 + B**2)
+khoang_cach = abs(A * h + B * k + C) / math.sqrt(A**2 + B**2)
 
 # Kiểm tra điều kiện tiếp tuyến
-if distance == r:
+if khoang_cach == r:
     print("Đường thẳng tiếp tuyến với đường tròn.")
 else:
     print("Đường thẳng không tiếp tuyến với đường tròn.")
